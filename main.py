f739c00c99cb42e9b864a9915f53ae5685ab2eed
Datsteelfn2 <Datsteelfn@gmail.com>
2025-02-13 21:13:56 -0500

    finished teacher and doctor classses, task done

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
 class job:
   name=None
   salary=None
-  hours_worked=None
-  def __init__(self,name,salary,hours_worked):
+  hours=None
+  def __init__(self,name,salary,hours):
     self.name=name
     self.salary=salary
-    self.hours=hours_worked
+    self.hours=hours
   def description(self):
     print(f"A {self.name} makes {self.salary} yearly and works {self.hours} hours per week")
-software_engineer=job("Software engineer","80,000",40)
-software_engineer.description()
+lawyer=job("lawyer","80,000",40)
+lawyer.description()
+print()
+
+class doctor(job):
+  speciality=None
+  years_of_experience=None
+  def __init__(self,speciality,years_of_experience):
+    self.speciality=speciality
+    self.years=years_of_experience
+    self.name="doctor"
+    self.salary="500,000"
+    self.hours="50"
+medic=doctor("Surgeon","50")
+medic.description()
+print(medic.speciality)
+print(medic.years)
+print()
+
+
+class teacher(job):
+  subject=None
+  position=None
+  def __init__(self,subject,position):
+    self.subject=subject
+    self.position=position
+    self.name="Computer science teacher"
+    self.salary="45,000"
+    self.hours="45"
+
+comp_sci=teacher("Comp science","Teacher")
+comp_sci.description()
+print(comp_sci.subject)
+print(comp_sci.position)
+print()
